6cc4d6bf6613d06f2e5005e0c4d42928cbd00bce
Naveed <60952170+naveed@users.noreply.github.com>
2020-09-27 00:24:32 -0400

    Updated file for dev4

---
+++ Hello World Console App 1/Program2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World_Console_App_1
+{
+    class Program2
+    {
+        void Main2(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is Feature 1 code");
+            Console.WriteLine("This is Feature 1 code 2");
+        }
+    }
+}
