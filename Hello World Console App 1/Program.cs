aacc7d37bd8d7a30ea4b0d283cfd5f16865794bf
Naveed <60952170+naveed@users.noreply.github.com>
2020-09-26 23:16:49 -0400

    Initial Branch

---
+++ Hello World Console App 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World_Console_App_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
