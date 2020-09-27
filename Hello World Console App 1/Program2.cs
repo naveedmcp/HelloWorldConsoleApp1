8700b0041ccd936e00e1945b3c982873b3bb2af1
Naveed <60952170+naveed@users.noreply.github.com>
2020-09-27 00:24:59 -0400

    updated program 2

--- Hello World Console App 1/Program2.cs
+++ Hello World Console App 1/Program2.cs
@@ -7,8 +7,6 @@ namespace Hello_World_Console_App_1
         void Main2(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("This is Feature 1 code");
-            Console.WriteLine("This is Feature 1 code 2");
         }
     }
 }
